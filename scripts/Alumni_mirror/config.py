8d07fb78b314167061678571537475e9071df64f
chrishaid <chaid@kippchicago.org>
2015-08-19 11:22:42 -0500

    Added Alumni DB scripts

---
+++ scripts/Alumni_mirror/config.py
@@ -0,0 +1,13 @@
+## Alumni Salesforce DB configurables
+SF_UID = 'you@example.org'
+SF_PWD = 'supersecurepassword'
+SF_TOKEN =  'superlongtokenfromSF'
+
+## Silo DB configurables
+DB_TYPE = 'mssql'
+DB_API = 'pyodbc'
+DB_DNS_NAME = 'Silo_Alumni_64'
+DB_SERVER = ''
+DB_NAME = 'Alumni_mirror'
+DB_USERNAME = ''
+DB_PASSWORD = ''
